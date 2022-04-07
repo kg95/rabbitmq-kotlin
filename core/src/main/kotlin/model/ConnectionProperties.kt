f5432b3b8e225f116dd926aed35b2db2651f15b8
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-07 11:47:26 +0200

    add ConnectionProperties data class

---
+++ core/src/main/kotlin/model/ConnectionProperties.kt
@@ -0,0 +1,10 @@
+package model
+
+data class ConnectionProperties(
+    val username: String,
+    val password: String,
+    val host: String,
+    val port: Int,
+    val virtualHost: String,
+    val isAutomaticRecoveryEnabled: Boolean
+)
