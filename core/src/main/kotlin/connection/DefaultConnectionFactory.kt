2f51219de0204446965d1a0d278d7423aa98cc00
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-07 10:41:33 +0200

    delete connection factories because of re-usability issue

--- core/src/main/kotlin/connection/DefaultConnectionFactory.kt
+++
@@ -1,5 +0,0 @@
-package connection
-
-class DefaultConnectionFactory(
-    rabbitMQConnectionFactory: com.rabbitmq.client.ConnectionFactory = com.rabbitmq.client.ConnectionFactory()
-): ConnectionFactory(rabbitMQConnectionFactory)
