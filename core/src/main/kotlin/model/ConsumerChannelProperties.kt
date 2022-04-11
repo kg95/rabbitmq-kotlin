00f6c604b15a08aae6ed756db13faaeabbcba829
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-11 17:07:02 +0200

    remove unused const variables from ConsumerChannelProperties

--- core/src/main/kotlin/model/ConsumerChannelProperties.kt
+++ core/src/main/kotlin/model/ConsumerChannelProperties.kt
@@ -2,8 +2,6 @@ package model
 
 private const val DEFAULT_PREFETCH_COUNT = 1000
 private const val DEFAULT_WATCH_DOG_INTERVAL_MILLIS = 5000L
-private const val CHANNEL_RENEW_TIMEOUT_MILLIS = 10000L
-private const val CHANNEL_RENEW_DELAY_MILLIS = 5000L
 
 data class ConsumerChannelProperties(
     val prefetchCount: Int = DEFAULT_PREFETCH_COUNT,
