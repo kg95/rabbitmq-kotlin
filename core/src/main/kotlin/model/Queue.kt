2d255238c4e000197fcb052967cccce109cbf7b9
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-11 17:07:50 +0200

    remove unused dtos

--- core/src/main/kotlin/model/Queue.kt
+++
@@ -1,6 +0,0 @@
-package model
-
-data class Queue(
-    val queueName: String,
-    val virtualHost: String
-)
--- core/src/main/kotlin/model/RabbitMQAccess.kt
+++
@@ -1,8 +0,0 @@
-package model
-
-data class RabbitMqAccess(
-    val username: String,
-    val password: String,
-    val host: String,
-    val port: Int,
-)
