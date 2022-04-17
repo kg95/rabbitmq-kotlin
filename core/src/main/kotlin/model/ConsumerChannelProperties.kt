b29e296747d47abbb6af669efceb6febbe2566e5
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-17 20:01:02 +0200

    remove unused dtos

--- core/src/main/kotlin/model/ConsumerChannelProperties.kt
+++
@@ -1,9 +0,0 @@
-package model
-
-private const val DEFAULT_PREFETCH_COUNT = 1000
-private const val DEFAULT_WATCH_DOG_INTERVAL_MILLIS = 5000L
-
-data class ConsumerChannelProperties(
-    val prefetchCount: Int = DEFAULT_PREFETCH_COUNT,
-    val watchDogIntervalMillis: Long = DEFAULT_WATCH_DOG_INTERVAL_MILLIS
-)
--- core/src/main/kotlin/model/ProducerProperties.kt
+++
@@ -1,9 +0,0 @@
-package model
-
-private const val DEFAULT_PUBLISH_ATTEMPT_COUNT = 1
-private const val DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS = 1000L
-
-data class ProducerProperties(
-    val publishAttemptCount: Int = DEFAULT_PUBLISH_ATTEMPT_COUNT,
-    val publishAttemptDelayMillis: Long = DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS,
-)
