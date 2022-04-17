6b1c4b2f7b3fe258b2ebd8d8f0bf0ccf114e772f
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-17 19:59:40 +0200

    refactor producer to throw exceptions instead of just logging

--- core/src/main/kotlin/RabbitMQProducer.kt
+++ core/src/main/kotlin/RabbitMQProducer.kt
@@ -1,31 +1,26 @@
 import channel.ProducerChannelProvider
 import com.rabbitmq.client.ReturnListener
 import kotlinx.coroutines.delay
-import org.slf4j.Logger
 import converter.Converter
-import exception.RabbitMqMessageReturnedException
 import kotlinx.coroutines.runBlocking
 import model.ConnectionProperties
-import model.ProducerProperties
 import util.convertToRabbitMQException
-import util.getLogger
+
+private const val DEFAULT_PUBLISH_ATTEMPT_COUNT = 1
+private const val DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS = 1000L
 
 class RabbitMQProducer<T: Any> (
     connectionProperties: ConnectionProperties,
     queueName: String,
     private val converter: Converter,
     private val type: Class<T>,
-    private val producerProperties: ProducerProperties = ProducerProperties(),
-    private val logger: Logger = getLogger(RabbitMQProducer::class.java)
+    private val publishAttemptCount: Int = DEFAULT_PUBLISH_ATTEMPT_COUNT,
+    private val publishAttemptDelayMillis: Long = DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS,
 ) {
     private var channelProvider: ProducerChannelProvider
 
     init {
-        val onReturn = ReturnListener { replyCode, replyText, exchange, routingKey, _, body ->
-            val message =
-                "Message returned from exchange: $exchange with routingKey: $routingKey ," +
-                        " replyCode: $replyCode replyText: $replyText, attempting to republish"
-            logger.error(message, RabbitMqMessageReturnedException(message))
+        val onReturn = ReturnListener { _, _, _, _, _, body ->
             runBlocking { publish(body) }
         }
         try {
@@ -54,9 +49,8 @@ class RabbitMQProducer<T: Any> (
     }
 
     private suspend fun publish(message: ByteArray) {
-        logger.debug("send message: {}", String(message, Charsets.UTF_8))
         var lastException: Throwable? = null
-        repeat(producerProperties.publishAttemptCount) {
+        repeat(publishAttemptCount) {
             try {
                 channelProvider.recreateChannel()
                 channelProvider.publish(message)
@@ -64,15 +58,18 @@ class RabbitMQProducer<T: Any> (
             } catch (e: Throwable) {
                 lastException = e
             }
-            delay(producerProperties.publishAttemptDelayMillis)
+            delay(publishAttemptDelayMillis)
         }
         lastException?.let {
-            logger.error("Failed to publish message ${producerProperties.publishAttemptCount} times", it)
-            throw it
+            throw convertToRabbitMQException(it)
         }
     }
 
     fun close() {
-        channelProvider.close()
+        try {
+            channelProvider.close()
+        } catch (_: Throwable) {
+            return
+        }
     }
 }
