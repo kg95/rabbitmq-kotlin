fba46f8d5e3b08d701c76685f12fe33e96148403
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-19 17:37:36 +0200

    add consumer and producer option dtos

---
+++ core/src/main/kotlin/model/ConsumerOptions.kt
@@ -0,0 +1,13 @@
+package model
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+private const val DEFAULT_PREFETCH_COUNT = 1000
+private const val DEFAULT_WATCH_DOG_INTERVAL_MILLIS = 10000L
+
+data class ConsumerOptions(
+    val dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    val prefetchCount: Int = DEFAULT_PREFETCH_COUNT,
+    val watchDogIntervalMillis: Long = DEFAULT_WATCH_DOG_INTERVAL_MILLIS
+)
---
+++ core/src/main/kotlin/model/ProducerOptions.kt
@@ -0,0 +1,9 @@
+package model
+
+private const val DEFAULT_PUBLISH_ATTEMPT_COUNT = 1
+private const val DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS = 1000L
+
+data class ProducerOptions(
+    val publishAttemptCount: Int = DEFAULT_PUBLISH_ATTEMPT_COUNT,
+    val publishAttemptDelayMillis: Long = DEFAULT_PUBLISH_ATTEMPT_DELAY_MILLIS
+)
