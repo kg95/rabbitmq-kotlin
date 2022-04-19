9e5bd4f1b6000bf0cf58e9f0b93548b09effd766
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-04-19 17:37:10 +0200

    add default values to RabbitMQAccess

--- core/src/main/kotlin/model/RabbitMQAccess.kt
+++ core/src/main/kotlin/model/RabbitMQAccess.kt
@@ -1,8 +1,8 @@
 package model
 
 data class RabbitMQAccess(
-    val username: String,
-    val password: String,
-    val host: String,
-    val port: Int
+    val username: String = "guest",
+    val password: String = "guest",
+    val host: String = "localhost",
+    val port: Int = 5672
 )
