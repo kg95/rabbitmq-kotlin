003ac9d32674a93a2bcb63ba5576b6c0ad4bb04f
KGAEHLSD <Kai.Gaehlsdorf@ottogroup.com>
2022-05-16 11:41:37 +0200

    migrate dependency versions to be up-to-date

--- rabbitmq-kotlin-lib/build.gradle.kts
+++ rabbitmq-kotlin-lib/build.gradle.kts
@@ -11,24 +11,25 @@ repositories {
 dependencies {
     //kotlin
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
 
     //rabbitmq java api
     implementation("com.rabbitmq:amqp-client:5.14.2")
 
     //kotlin coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 
     //jackson serialization
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
 
     //Test
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("io.mockk:mockk:1.10.0")
-    testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
 }
 
 tasks.test {
